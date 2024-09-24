951b440ba0685c5f23c7a4ab9544808cff6ce046
tranba <janetran251@gmail.com>
2024-09-24 23:19:38 +1000

    Second commit

--- components/mini-components/Highlights1.tsx
+++ components/mini-components/Highlights1.tsx
@@ -2,11 +2,18 @@
 import React, {useState} from 'react'
 import Image from 'next/image'
 import { images } from '../../utils/constants'
+import { IoIosArrowRoundBack, IoIosArrowRoundForward } from "react-icons/io";
 
 const Hightlights1 = () => {
-
+  const [activeIndex,setActiveIndex]= useState(0);
+  const handleNext =()=>{
+    setActiveIndex((prevIndex)=>(prevIndex+1)% images.length)
+  };
+  const handlePrev =()=>{
+    setActiveIndex((prevIndex)=>(prevIndex-1+images.length)%images.length)
+  }
   return (
-    <div className='bg-white pt-[100px] relative'> 
+    <div className='bg-white pt-[100px] relative overflow-hidden'> 
       {/* This is the highlight text */}
       <div className='space-y-4 absolute z-10 pl-24'>
           <p className='text-7xl text-sky-600 font-bold font-custom'>Highlights</p>
@@ -22,27 +29,33 @@ const Hightlights1 = () => {
           alt='hightlight background'
           />
         </div>
-        <div className='absolute top-[30px] left-[350px]'>
+        <div className='absolute top-[30px] left-[350px] w-full'>
                  {/* This is the slide */}
-                <div className='absolute top-0 '>
+                <div className='absolute top-0 flex w-full'>
                       {/* This is one box of slide */}
-                      <div className='w-[660px] h-[440px] relative shadow-lg rounded-l-full  overflow-hidden'>
-                            <div className='relative'>
-                                <Image src= "/travel.1.png" width={660} height={400} alt='travel' className='rounded-r-3xl'/>
+                    
+                            {images.map((image,index)=>(
+                            <div className={`w-[660px] h-[440px] relative shadow-lg rounded-l-full`}>
+                                <div className='relative w-[660px] h-[440px]'>
+                                    <Image src= {image.src} layout='fill' objectFit='cover' alt='travel' className='rounded-r-3xl'/>
+                                </div>
+                                <div className='absolute bottom-0'>
+                                  <Image src="/curved container.png" width={660} height={320} alt='travel' className='rounded-r-3xl'/>
+                                </div>
+                                <div className='absolute inset-y-2/4 pl-[30px] pt-[35px] '>
+                                  <p className='font-custom text-sky-600 text-xl'>Lorem ipsum dolor<br/> amet, consectetur</p>
+                                  <div className=' text-black/[0.3] text-base font-semibold pt-[15px]'>
+                                    <p className='pl-20'>{image.desc}</p>
+                                  </div>
+                                </div>
                             </div>
-                            <div className='absolute bottom-0'>
-                              <Image src="/curved container.png" width={660} height={320} alt='travel' className='rounded-r-3xl'/>
+                            ))}
+                            <div className='absolute z-10 left-2/3 top-1/2'>
+                              <button key={""} onClick={handleNext} className={"w-[110px] h-[110px] rounded-full flex justify-center items-center bg-white"}><IoIosArrowRoundForward size={40} color='sky'/></button>
                             </div>
-                            <div className='absolute inset-y-2/4 pl-[30px] pt-[35px] '>
-                              <p className='font-custom text-sky-600 text-xl'>Lorem ipsum dolor<br/> amet, consectetur</p>
-                              <div className=' text-black/[0.3] text-base font-semibold pt-[15px]'>
-                                <p className='pl-[25px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores accusantium </p>
-                                <p className='pl-[45px]'>deleniti est quis. Dolor hic autem blanditiis animi velit in aliquid voluptas</p>
-                                <p className='pl-[70px]'> expedita obcaecati. Eveniet commodi enim quod iste debitis? Donec</p>
-                                <p className='pl-[115px]'> bibendum augue porta, convallis nunc at, aliquet mauris</p>
-                              </div>
+                            <div className='absolute z-10 left-0 -bottom-20 '>
+                              <button key={""} onClick={handleNext} className={"w-[110px] h-[110px] rounded-full flex justify-center items-center bg-gray-200"}><IoIosArrowRoundBack size={40} color='sky'/></button>
                             </div>
-                      </div>
                 </div>
         </div>
       </div>
--- utils/constants.tsx
+++ utils/constants.tsx
@@ -1,44 +1,44 @@
 export const images=[
     {
-        id:1,
+        id:0,
         src:"/travel.1.png",
         title:"family travel",
-        desc:"a family in the airport"
+        desc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur iste unde et nesciunt officia numquam exercitationem temporibus, ex omnis odio provident, laboriosam alias incidunt nobis? Est aliquam beatae provident a!"
     },
     {
-        id:2,
-        src:"/travel.2.png",
+        id:1,
+        src:"/travel.2.jpg",
         title:"lounge",
-        desc:"a fancy lounge"
+        desc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur iste unde et nesciunt officia numquam exercitationem temporibus, ex omnis odio provident, laboriosam alias incidunt nobis? Est aliquam beatae provident a!"
     },
-    {
-        id:3,
-        src:"/travel.3.png",
-        title:"shopping at airport",
-        desc:"a man standing in front of an airport store"
-    },
-    {
-        id:4,
-        src:"/travel.4.png",
-        title:"fancy store",
-        desc:"a fancy store"
-    },
-    {
-        id:5,
-        src:"/travel.5.png",
-        title:"fancy store",
-        desc:"a fancy store"
-    },
-    {
-        id:6,
-        src:"/travel.6.png",
-        title:"family travel",
-        desc:"family travel"
-    },
-    {
-        id:7,
-        src:"/travel.7.png",
-        title:"family travel",
-        desc:"family travel"
-    }
+    // {
+    //     id:2,
+    //     src:"/travel.3.png",
+    //     title:"shopping at airport",
+    //     desc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur iste unde et nesciunt officia numquam exercitationem temporibus, ex omnis odio provident, laboriosam alias incidunt nobis? Est aliquam beatae provident a!"
+    // },
+    // {
+    //     id:3,
+    //     src:"/travel.4.png",
+    //     title:"fancy store",
+    //     desc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur iste unde et nesciunt officia numquam exercitationem temporibus, ex omnis odio provident, laboriosam alias incidunt nobis? Est aliquam beatae provident a!"
+    // },
+    // {
+    //     id:4,
+    //     src:"/travel.5.jpg",
+    //     title:"fancy store",
+    //     desc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur iste unde et nesciunt officia numquam exercitationem temporibus, ex omnis odio provident, laboriosam alias incidunt nobis? Est aliquam beatae provident a!"
+    // },
+    // {
+    //     id:5,
+    //     src:"/travel.6.jpg",
+    //     title:"family travel",
+    //     desc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur iste unde et nesciunt officia numquam exercitationem temporibus, ex omnis odio provident, laboriosam alias incidunt nobis? Est aliquam beatae provident a!"
+    // },
+    // {
+    //     id:6,
+    //     src:"/travel.7.jpg",
+    //     title:"family travel",
+    //     desc:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur iste unde et nesciunt officia numquam exercitationem temporibus, ex omnis odio provident, laboriosam alias incidunt nobis? Est aliquam beatae provident a!"
+    // }
 ]
